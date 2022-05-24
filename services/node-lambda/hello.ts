50d041cf0db69f230c7af5e10492b5520f30482e
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-05-24 22:23:21 +0500

    add local debugging

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -5,6 +5,7 @@ const s3Client = new S3();
 
 async function handler(event: any, context: any) {
   const buckets = await s3Client.listBuckets().promise();
+  console.log(buckets);
   return {
     statusCode: 200,
     body: 'here are your buckets: ' + JSON.stringify(buckets.Buckets),
