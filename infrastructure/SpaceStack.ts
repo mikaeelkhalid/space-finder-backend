2f9564e92f3d485ab99ed2551739b05d16559541
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-05-24 14:33:08 +0500

    hello api lambda integration

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -6,16 +6,24 @@ import {
   Runtime,
 } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, 'SpaceApi');
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    const lambdaFunction = new LambdaFunction(this, 'helloLambda', {
+    // basic hello lambda
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main',
     });
+
+    // hello api lambda integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
 }
