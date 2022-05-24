d3300821a56a1082a8171df8724110ef52afdb76
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-05-24 14:11:12 +0500

    deploying hello lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,10 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
 
 export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const lambdaFunction = new LambdaFunction(this, 'helloLambda', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main',
+    });
   }
 }
