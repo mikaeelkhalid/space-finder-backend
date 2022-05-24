8c1c4e9c51e96268b2e5f2949fc7f2116d490833
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-05-24 21:59:21 +0500

    list s3 buckets

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -9,6 +9,7 @@ import { join } from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, 'SpaceApi');
@@ -24,6 +25,12 @@ export class SpaceStack extends Stack {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler',
     });
+
+    // add a policy to the lambda to allow it to access the s3 bucket
+    const s3ListBucketsPolicy = new PolicyStatement();
+    s3ListBucketsPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListBucketsPolicy.addResources('*');
+    helloNodeLambda.addToRolePolicy(s3ListBucketsPolicy);
 
     // hello api lambda integration
     const helloLambdaIntegration = new LambdaIntegration(helloNodeLambda);
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,10 +1,13 @@
 import { v4 as uuid } from 'uuid';
+import { S3 } from 'aws-sdk';
+
+const s3Client = new S3();
 
 async function handler(event: any, context: any) {
-  console.log('An event: ', event);
+  const buckets = await s3Client.listBuckets().promise();
   return {
     statusCode: 200,
-    body: 'Hello World! ' + uuid(),
+    body: 'here are your buckets: ' + JSON.stringify(buckets.Buckets),
   };
 }
 
